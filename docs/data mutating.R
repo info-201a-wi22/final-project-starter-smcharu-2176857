f1aa18f2d6362b9e1007fffe54bc6803955b5ec9
Saimanasvi <smcharu@uw.edu>
2022-02-15 19:28:37 -0800

    add data_set 2

--- docs/data mutating.R
+++ docs/data mutating.R
@@ -1,1 +1,2 @@
-https://data.cdc.gov/api/views/b58h-s9zx/rows.csv?accessType=DOWNLOAD 
+data_1 <- read.csv("https://data.cdc.gov/api/views/b58h-s9zx/rows.csv?accessType=DOWNLOAD", header = TRUE, stringsAsFactors = FALSE)
+data_2 <- read.csv("https://api.covidtracking.com/v1/states/current.csv", header = TRUE, stringsAsFactors = FALSE)
