5eaf41f9e70988e0e8c45e34fe2ae9e608bdd113
Saimanasvi <smcharu@uw.edu>
2022-02-15 20:13:10 -0800

    smcharu tried to mutate data 1 file. FAILED

--- docs/data mutating.R
+++ docs/data mutating.R
@@ -1,8 +1,21 @@
-data_1 <- read.csv("https://data.cdc.gov/api/views/b58h-s9zx/rows.csv?accessType=DOWNLOAD", header = TRUE, stringsAsFactors = FALSE)
-data_2 <- read.csv("https://api.covidtracking.com/v1/states/current.csv", header = TRUE, stringsAsFactors = FALSE)
+df1 <- read.csv("https://data.cdc.gov/api/views/b58h-s9zx/rows.csv?accessType=DOWNLOAD", header = TRUE, stringsAsFactors = FALSE)
+df2 <- read.csv("https://api.covidtracking.com/v1/states/current.csv", header = TRUE, stringsAsFactors = FALSE)
 
-data_1_state <- data_1 %>% 
+# Trying to get rid of dollar sign and spaces in X1st.Round.Payment bc you cant
+# add up the values with the dollar sign and spaces. Theres error 
+# "Error: unexpected ',' in "data_1$X1st.Round.Payment <- ("$"," but idk why
+
+data_1$X1st.Round.Payment <- ("$   ", "", as.character(df1$X1st.Round.Payment))
+
+#Example from internet for what I did above
+df1$x1<-gsub("1","",as.character(df1$x1))
+
+# Trying to get sum of values, but I can't because of the stupid dollar and spaces
+# It should work otherwise? idfk anymore man
+df1_state <- df_1 %>% 
   group_by(State) %>% 
+  select(-c(Returned..1st.Payment.)) %>% 
+  
   summarize(
     first_round_total <- sum(X1st.Round.Payment, na.rm = TRUE)
   ) 
