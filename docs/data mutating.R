5e062e821e535a3a7b80595c458b53824fdbb21e
Saimanasvi <smcharu@uw.edu>
2022-02-15 19:52:33 -0800

    attempt of mutating dataset 1

--- docs/data mutating.R
+++ docs/data mutating.R
@@ -1,2 +1,8 @@
 data_1 <- read.csv("https://data.cdc.gov/api/views/b58h-s9zx/rows.csv?accessType=DOWNLOAD", header = TRUE, stringsAsFactors = FALSE)
 data_2 <- read.csv("https://api.covidtracking.com/v1/states/current.csv", header = TRUE, stringsAsFactors = FALSE)
+
+data_1_state <- data_1 %>% 
+  group_by(State) %>% 
+  summarize(
+    first_round_total <- sum(X1st.Round.Payment, na.rm = TRUE)
+  ) 
