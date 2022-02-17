7fa4341d496074d54c66ded4d162e44d179aabbc
Saimanasvi <smcharu@uw.edu>
2022-02-17 00:53:15 -0800

    Final version

--- docs/data mutating.R
+++ docs/data mutating.R
@@ -15,8 +15,7 @@ df1$X2nd.Round.Payment <- c(df1$X2nd.Round.Payment)
 df1$X2nd.Round.Payment <- gsub("[$,]","",df1$X2nd.Round.Payment)
 df1$X2nd.Round.Payment <- as.numeric(df1$X2nd.Round.Payment)
 
-# Trying to get sum of values, but I can't because of the stupid dollar and spaces
-# It should work otherwise? idfk anymore man
+# Data Frame with total sum of payment for each state
 df1_state <- df1 %>% 
   group_by(State) %>% 
   select(-c(Returned..1st.Payment.)) %>%
@@ -31,3 +30,5 @@ df2 <- df2 %>%
 
 #Combining df1 and df2
 combined_data <- left_join(df1_state, df2, by = c("State"))
+
+#June 10 , 2020
