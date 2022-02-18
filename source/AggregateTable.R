36bd76fa9d6e6f868c1efff355275970fde9fa5d
niyat efrem <niyate@uw.edu>
2022-02-18 01:37:25 -0800

    smcharu changes

--- source/AggregateTable.R
+++ source/AggregateTable.R
@@ -4,13 +4,13 @@ hospital_df <- read.csv("https://healthdata.gov/resource/g62h-syeh.csv", header 
 #DATA WRANGLING
 # Getting rid of commas and dollar signs in X1st.Round.Payment
 funds_df$X1st.Round.Payment <- c(funds_df$X1st.Round.Payment)
-funds_df$X1st.Round.Payment <- gsub("[$,]","",funds_df$X1st.Round.Payment)
+funds_df$X1st.Round.Payment <- gsub("[$,]", "", funds_df$X1st.Round.Payment)
 funds_df$X1st.Round.Payment <- as.numeric(funds_df$X1st.Round.Payment)
 
 # Getting rid of commas and dollar signs in X2nd.Round.Payment
 
 funds_df$X2nd.Round.Payment <- c(funds_df$X2nd.Round.Payment)
-funds_df$X2nd.Round.Payment <- gsub("[$,]","",funds_df$X2nd.Round.Payment)
+funds_df$X2nd.Round.Payment <- gsub("[$,]", "", funds_df$X2nd.Round.Payment)
 funds_df$X2nd.Round.Payment <- as.numeric(funds_df$X2nd.Round.Payment)
 
 library(stringr)
@@ -21,40 +21,41 @@ library(dplyr)
 #
 #
 
-# Table with each state, total amount of funding they got, and the percentage 
+# Table with each state, total amount of funding they got, and the percentage
 # of the total that it is
-state_total_funding <- funds_df %>% 
-  group_by(State) %>% 
-  summarize(total_funds = sum(X2nd.Round.Payment, na.rm = TRUE)) %>% 
-  mutate(us_total = sum(total_funds)) %>% 
+state_total_funding <- funds_df %>%
+  group_by(State) %>%
+  summarize(total_funds = sum(X2nd.Round.Payment, na.rm = TRUE)) %>%
+  mutate(us_total = sum(total_funds)) %>%
   mutate(
     percentage_of_funds = paste(round((total_funds / us_total) * 100, 2), "%")
-    ) %>% 
+    ) %>%
   select(-c(us_total))
   
 
 #Data Wrangling to make data sets usable
-hospital_df$date <- str_sub(hospital_df$date, end = -14) 
-hospital_df <- hospital_df %>% 
-  mutate(year = substr(hospital_df$date, start = 1, stop = 4)) %>% 
+hospital_df$date <- str_sub(hospital_df$date, end = -14)
+hospital_df <- hospital_df %>%
+  mutate(year = substr(hospital_df$date, start = 1, stop = 4)) %>%
   select(year, state, inpatient_beds_used_covid)
 
 # Table with each state, total amount of funding they got, and the pecentage
 # of the total that it is
-total_patient_comparision <- hospital_df %>% 
-  group_by(state) %>% 
-  summarize(state_total_patients = sum(inpatient_beds_used_covid, na.rm = TRUE)) %>% 
-  mutate(us_total_patients = sum(state_total_patients)) %>% 
+total_patient_comparision <- hospital_df %>%
+  group_by(state) %>%
+  summarize(state_total_patients =
+              sum(inpatient_beds_used_covid, na.rm = TRUE)) %>%
+  mutate(us_total_patients = sum(state_total_patients)) %>%
   mutate(
-    percentage_of_patients = 
+    percentage_of_patients =
       paste(round((state_total_patients / us_total_patients) * 100, 2), "%")
-  ) %>% 
+    ) %>%
   select(-c(us_total_patients))
 
 # Renaming column to join data sets by State
-total_patient_comparision <- total_patient_comparision %>% 
+Total_patient_comparision <- total_patient_comparision %>%
   rename(State = state)
 
-# Joining datasets together 
-patient_funding_comparision <- 
+# Joining datasets together
+patient_funding_comparision <-
   left_join(state_total_funding, total_patient_comparision, by = c("State"))
